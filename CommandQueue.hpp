49a35946105bc75f3dd4107342caeb4703ee032d
therselman <therselman@gmail.com>
2016-08-25 12:23:43 +0200

    Add files via upload

--- CommandQueue.hpp
+++ CommandQueue.hpp
@@ -253,6 +253,74 @@ protected:																								//	protected - incase you want to extend it, s
 	}
 
 
+	//
+	//		returns() Stub functions																	//	These are the `stub` functions that actually CALL YOUR function and returns the value directly to the address you specified! These are the functions that are actually called on another thread! They are called directly by the thread inner-loop!
+	//
+	template< typename TCB, typename R >
+	static void returnStubV0( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function();
+	}
+	template< typename TCB, typename R, typename T1 >
+	static void returnStubV1( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1 );
+	}
+	template< typename TCB, typename R, typename T1, typename T2 >
+	static void returnStubV2( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		const T2 v2 = *( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1, v2 );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3 >
+	static void returnStubV3( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		const T2 v2 = *( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) );
+		const T3 v3 = *( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1, v2, v3 );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4 >
+	static void returnStubV4( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		const T2 v2 = *( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) );
+		const T3 v3 = *( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) );
+		const T4 v4 = *( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1, v2, v3, v4 );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4, typename T5 >
+	static void returnStubV5( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		const T2 v2 = *( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) );
+		const T3 v3 = *( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) );
+		const T4 v4 = *( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) );
+		const T5 v5 = *( ( T5* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1, v2, v3, v4, v5 );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6 >
+	static void returnStubV6( char* data )
+	{
+		const TCB function = *( ( TCB* ) data );
+		const T1 v1 = *( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) );
+		const T2 v2 = *( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) );
+		const T3 v3 = *( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) );
+		const T4 v4 = *( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) );
+		const T5 v5 = *( ( T5* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) ) );
+		const T6 v6 = *( ( T6* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) + sizeof( T5 ) ) );
+		**( ( R* ) ( data + sizeof( TCB* ) ) ) = function( v1, v2, v3, v4, v5, v6 );
+	}
+
+
 public:
 	//
 	//		constructors
@@ -359,6 +427,109 @@ public:
 		*( ( T4* ) ( data + sizeof( TCB* ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) ) = v4;
 		*( ( T5* ) ( data + sizeof( TCB* ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) ) ) = v5;
 		*( ( T6* ) ( data + sizeof( TCB* ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) + sizeof( T5 ) ) ) = v6;
+
+		releaseBuffer( buffer );
+	}
+
+
+	//
+	//		returns()																					//	We store the return address directly after the function pointer address, the `stub` functions are what actually call your function, they are the ones that are actually executed on another thread!
+	//
+	template< typename TCB, typename R >
+	void returns( const TCB function, const R ret )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV0< TCB, R >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;													//	We store the return address on our internal data buffer, directly after the function call address
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1 >
+	void returns( const TCB function, const R ret, const T1 v1 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV1< TCB, R, T1 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1, typename T2 >
+	void returns( const TCB function, const R ret, const T1 v1, const T2 v2 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV2< TCB, R, T1, T2 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+		*( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) ) = v2;
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3 >
+	void returns( const TCB function, const R ret, const T1 v1, const T2 v2, const T3 v3 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV3< TCB, R, T1, T2, T3 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+		*( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) ) = v2;
+		*( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) ) = v3;
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4 >
+	void returns( const TCB function, const R ret, const T1 v1, const T2 v2, const T3 v3, const T4 v4 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV4< TCB, R, T1, T2, T3, T4 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+		*( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) ) = v2;
+		*( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) ) = v3;
+		*( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) ) = v4;
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4, typename T5 >
+	void returns( const TCB function, const R ret, const T1 v1, const T2 v2, const T3 v3, const T4 v4, const T5 v5 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV5< TCB, R, T1, T2, T3, T4, T5 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) + sizeof( T5 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+		*( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) ) = v2;
+		*( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) ) = v3;
+		*( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) ) = v4;
+		*( ( T5* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) ) ) = v5;
+
+		releaseBuffer( buffer );
+	}
+	template< typename TCB, typename R, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6 >
+	void returns( const TCB function, const R ret, const T1 v1, const T2 v2, const T3 v3, const T4 v4, const T5 v5, const T6 v6 )
+	{
+		queue_buffer_t* buffer = acquireBuffer();
+
+		char* data = allocCommand( buffer, returnStubV6< TCB, R, T1, T2, T3, T4, T5, T6 >, sizeof( PFNCommandHandler* ) + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) + sizeof( T5 ) + sizeof( T6 ) );
+		*( ( TCB* ) data ) = function;
+		*( ( R* ) ( data + sizeof( TCB* ) ) ) = ret;
+		*( ( T1* ) ( data + sizeof( TCB* ) + sizeof( R ) ) ) = v1;
+		*( ( T2* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) ) ) = v2;
+		*( ( T3* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) ) ) = v3;
+		*( ( T4* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) ) ) = v4;
+		*( ( T5* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) ) ) = v5;
+		*( ( T6* ) ( data + sizeof( TCB* ) + sizeof( R ) + sizeof( T1 ) + sizeof( T2 ) + sizeof( T3 ) + sizeof( T4 ) + sizeof( T5 ) ) ) = v6;
 
 		releaseBuffer( buffer );
 	}
